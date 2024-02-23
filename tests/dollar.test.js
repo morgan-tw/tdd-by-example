8f813196f2eb1094aa8fc22e5d0e99e10daba63d
Morgan Amouroux <64392532+morgan-tw@users.noreply.github.com>
2024-02-23 15:04:44 +0700

    Private amount - Refactor tests to not use amount anymore

--- tests/dollar.test.js
+++ tests/dollar.test.js
@@ -4,16 +4,13 @@ describe("dollar", () => {
     describe("multiplication", () => {
         it("returns a new value whose amount is the result of the multiplication", () => {
             const five = new Dollar(5);
-            const product = five.times(2);
-            expect(product.amount).toEqual(10);
+            expect(five.times(2)).toEqual(new Dollar(10));
         });
 
         it("preservs original dollar", () => {
             const five = new Dollar(5);
-            let product = five.times(2);
-            expect(product.amount).toEqual(10);
-            product = five.times(3);
-            expect(product.amount).toEqual(15);
+            expect(five.times(2)).toEqual(new Dollar(10));
+            expect(five.times(3)).toEqual(new Dollar(15));
         });
     });
 
