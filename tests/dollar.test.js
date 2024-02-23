974ea2261f68a5914b9c3a75647710a11db2388b
Morgan Amouroux <64392532+morgan-tw@users.noreply.github.com>
2024-02-23 14:47:13 +0700

    Value object - Make it fail

--- tests/dollar.test.js
+++ tests/dollar.test.js
@@ -7,5 +7,13 @@ describe("dollar", () => {
             five.times(2);
             expect(five.amount).toEqual(10);
         });
+
+        it("preserves original dollar", () => {
+            const five = new Dollar(5);
+            five.times(2);
+            expect(five.amount).toEqual(10);
+            five.times(3);
+            expect(five.amount).toEqual(15);
+        });
     });
 });
